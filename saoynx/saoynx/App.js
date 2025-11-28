9c6a9d0a225cd67c9b44669a5899cfebe4e48a03
taurinrobinson-wq <taurinrobinson@gmail.com>
2025-11-28 22:28:19 +0000

    fix(app): remove duplicate StatusBar import in App.js

--- saoynx/saoynx/App.js
+++ saoynx/saoynx/App.js
@@ -1,9 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import MessageOverlay from './src/screens/MessageOverlay';
 
 export default function App() {
