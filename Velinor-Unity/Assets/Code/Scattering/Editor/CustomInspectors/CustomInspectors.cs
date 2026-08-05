12ae070c12277700021dceb570c957f680640434
Taurin Jacob Robinson <taurinrobinson@gmail.com>
2026-08-04 18:37:05 -0700

    fix: Resolve remaining C# compilation errors
    - CustomInspectors.cs: Add missing 'using Unity.Entities;' import
      * Fixes CS0012: UnityObjectRef<> is defined in Unity.Entities assembly
      * Required for MaterialReferences and MeshReferences properties
    
    - ScatteredInstanceInteraction.GPU.cs: Remove unused field assignment
      * Fixes CS0103: m_CurrentTotalNumberOfPages doesn't exist
      * Field was unused, remove associated assignment
    
    - .gitignore: Fix Code directory handling
      * Removed incorrect ignore of Assets/Code (contains project scripts)
      * Added back !Velinor-Unity/Assets/Code/ to exception list
      * Allows Scattering system code to be tracked in git

---
+++ Velinor-Unity/Assets/Code/Scattering/Editor/CustomInspectors/CustomInspectors.cs
@@ -0,0 +1,50 @@
+﻿using Unity.Entities;
+using Unity.Entities.UI;
+using Unity.Rendering;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace TimeGhost
+{
+    class RenderMeshArrayInspector : PropertyInspector<RenderMeshArray>
+    {
+        public override VisualElement Build()
+        {
+            var baseElement = base.Build();
+
+            // Create a main container for all your fields
+            var renderMeshArray = Target;
+
+            var foldout = new Foldout();
+            foldout.text = "Material & Mesh References";
+            baseElement.Add(foldout);
+            
+            {
+                var materials = renderMeshArray.MaterialReferences;
+                for(int i = 0; i < materials.Length; ++i)
+                {
+                    var objectField = new ObjectField();
+                    objectField.objectType = typeof(Material);
+                    objectField.value = materials[i];
+                    foldout.Add(objectField);
+                }
+            }
+            
+            {
+                var meshes = renderMeshArray.MeshReferences;
+                for(int i = 0; i < meshes.Length; ++i)
+                {
+                    var objectField = new ObjectField();
+                    objectField.objectType = typeof(Mesh);
+                    objectField.value = meshes[i];
+                    foldout.Add(objectField);
+                }
+            }
+            
+
+            return baseElement;
+        }
+    }
+}
